cf2052f5e25ddd1914077a1307056cc11e37930e
huynguyen0903-hub <huynhgcs18412@fpt.edu.vn>
2022-03-19 12:45:51 +0700

    version0

---
+++ index.js
@@ -0,0 +1,5 @@
+document.querySelector('#signup-form .actions ul li:last-child a').onclick = function(){
+    // let btnSubmit = document.querySelector('#signup-form ul li:last-child a');
+    
+             
+}
