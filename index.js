e43a363f3a842a04d5968c6cbfb638b9a3533dbe
huynguyen0903-hub <huynhgcs18412@fpt.edu.vn>
2022-03-22 13:19:23 +0700

    update bai tap

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 document.querySelector('#signup-form .actions ul li:last-child a').onclick = function(){
     // let btnSubmit = document.querySelector('#signup-form ul li:last-child a');
+    var arrInput = document.querySelectorAll("#signup-form input, #signup-form select");
     
-             
+    let nguoiDung = {};
+    for (let input of arrInput){
+        let {id,value,style} = input;
+        nguoiDung = {...nguoiDung,[id]:value}
+
+    }
+    let contentHTML = '';
+    for (let  key in nguoiDung){
+        contentHTML += `
+        <tr>
+            <td>${key}</td>
+            <td>${nguoiDung[key]}</td>
+            </tr>
+        `
+    }
+    document.querySelector('tbody').innerHTML = contentHTML;
 }
